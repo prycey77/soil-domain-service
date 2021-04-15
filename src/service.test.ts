998d54d85e9d28d2d56b2ecec6d47cbe573b98f5
Richard <Richard.Pryce@bardsley-england.com>
2021-04-15 16:56:45 +0100

    Added 'resolveJsonMondule: true to tsconfig.json. Service.test still WIP

--- src/service.test.ts
+++ src/service.test.ts
@@ -1,4 +1,5 @@
-import { S3Event } from "aws-lambda";
+import { S3Event, Context } from "aws-lambda";
+import bodyJson from "./trigger.json";
 import { uploadSoilSampleToDdb } from "./service";
 import { dynamoLoader } from "./ddbLoader";
 import { getS3Data } from "./getS3Object";
@@ -7,18 +8,34 @@ import { getS3Data } from "./getS3Object";
 jest.mock("./getS3Object");
 jest.mock("./ddbLoader");
 
-const body = Buffer.from(JSON.stringify(bodyJson)).toString("base64");
+const emptyContext: Context = {
+  callbackWaitsForEmptyEventLoop: false,
+  functionName: "",
+  functionVersion: "",
+  invokedFunctionArn: "",
+  memoryLimitInMB: "",
+  awsRequestId: "",
+  logGroupName: "",
+  logStreamName: "",
+  getRemainingTimeInMillis: () => 1,
+  done: () => {},
+  fail: () => {},
+  succeed: () => {},
+};
 
 function mockFunction<T extends (...args: any[]) => any>(fn: T): jest.MockedFunction<T> {
   return fn as jest.MockedFunction<T>;
 }
 
-const dynamoLoaderMock = mockFunction(dynamoLoader);
+const body = JSON.parse(bodyJson);
+
 const getS3DataMock = mockFunction(getS3Data);
+const dynamoLoaderMock = mockFunction(dynamoLoader);
 
 // getS3DataMock.mockImplementation;
-test("Upload Soil Sample Lambda Test", () => {
-  const event = {} as any;
-
-  const result = await uploadSoilSampleToDdb(event, {}, null);
+test("Upload Soil Sample Lambda Test", async () => {
+  const event: S3Event = { body } as any;
+  getS3DataMock.mockReturnValue(Promise.resolve({ test: "test" }));
+  const response = await uploadSoilSampleToDdb(event, emptyContext, () => {});
+  expect(dynamoLoaderMock).toBeCalledTimes(1);
 });
