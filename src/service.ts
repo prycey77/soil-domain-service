d695cac5816df9c2c008225b9c4a54b56ff08296
Mark Smithson <mark.smithson@bardsley-england.com>
2021-04-19 08:26:41 +0100

    remove redundant try/catch simplify code

--- src/service.ts
+++ src/service.ts
@@ -1,24 +1,15 @@
-/* eslint-disable no-console */
 import { S3Handler } from "aws-lambda";
-import { dynamoLoader } from "./ddbLoader";
-import { getS3Data } from "./getS3Object";
+import { saveItems } from "./database";
+import { getJsonObject } from "./objectStore";
 
-const uploadSoilSampleToDdb: S3Handler = async (event) => {
-  const { name } = event.Records[0].s3.bucket;
-  const { key } = event.Records[0].s3.object;
+const saveSoilSample: S3Handler = async (event) => {
+  const { name: Bucket } = event.Records[0].s3.bucket;
+  const { key: Key } = event.Records[0].s3.object;
 
-  const getObjparams = {
-    Bucket: name,
-    Key: key,
-  };
-
-  try {
-    const dataJson = await getS3Data(getObjparams);
-    await dynamoLoader("eurofins-monitor-results", dataJson);
-    console.log("Success!");
-  } catch (error) {
-    return error;
-  }
+  const dataJson = await getJsonObject({ Bucket, Key });
+  await saveItems(dataJson);
+  // eslint-disable-next-line no-console
+  console.log("Success!");
 };
 
-export { uploadSoilSampleToDdb };
+export { saveSoilSample };
