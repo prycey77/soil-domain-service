cbb89713a28da2c22d3138c53672b5c7e1b1b385
Mark Smithson <mark.smithson@bardsley-england.com>
2021-04-19 08:24:03 +0100

    remove unecessary mock

--- __mocks__/dynamodb.ts
+++
@@ -1,8 +0,0 @@
-
-export const awsSdkPrimoseResponse = jest.fn().mockReturnValue(Promise.resolve(true));
-
-const putFn = jest.fn().mockImplementation(() => ({ promise: awsSdkPrimoseResponse}));
-
-export class DocumentClient {
-  put = putFn
-}
