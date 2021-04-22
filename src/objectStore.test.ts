299748988a926e22aa2dbb909701174054056f29
Richard <Richard.Pryce@bardsley-england.com>
2021-04-22 14:49:32 +0100

    refactor

--- src/objectStore.test.ts
+++ src/objectStore.test.ts
@@ -6,40 +6,46 @@ const testData = {
   OmTypeMonster: "Loess",
   DtMonster: "8102020",
 };
+
+const params = { Bucket: "testBucket", Key: "testKey" };
+const promiseFn = jest.fn();
+
 jest.mock("aws-sdk", () => {
   return {
-    // eslint-disable-next-line no-return-assign
     S3: jest.fn(() => ({
-      getObject: jest.fn(() => ({
-        promise: () =>
-          Promise.resolve({
-            Body: JSON.stringify(testData),
-          }),
-      })),
+      getObject: () => {
+        return {
+          promise: promiseFn,
+        };
+      },
     })),
   };
 });
 
 describe("mock s3 get object call", () => {
-  const params = { Bucket: "testBucket", Key: "testKey" };
+  beforeEach(() => {
+    promiseFn.mockReset();
+  });
   test("test s3 is returning data", async () => {
-    const res = await getJsonObject(params);
+    let res;
+    promiseFn.mockReturnValue(Promise.resolve({ Body: JSON.stringify(testData) }));
+
+    try {
+      res = await getJsonObject(params);
+    } catch (e) {
+      console.log(`Error found ${e}`);
+    }
     expect(res).toStrictEqual(testData);
   });
-  test("error if s3 is not returning json", async () => {
-    jest.resetAllMocks();
-    jest.mock("aws-sdk", () => {
-      return {
-        // eslint-disable-next-line no-return-assign
-        S3: jest.fn(() => ({
-          getObject: jest.fn(() => ({
-            promise: () => Promise.resolve({ Body: "not Json" }),
-          })),
-        })),
-      };
-    });
 
-    const res = await getJsonObject(params);
+  test("error if s3 is not returning json", async () => {
+    let res;
+    promiseFn.mockReturnValue(Promise.resolve({ Body: "not Json" }));
+    try {
+      res = await getJsonObject(params);
+    } catch (e) {
+      console.log(`Error found ${e}`);
+    }
     const err = new Error("Not Json");
     expect(res).toEqual(err);
   });
