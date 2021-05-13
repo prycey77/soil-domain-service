2e580fcc25886b668116d708ce46cff8c9f0f658
Mark Smithson <mark.smithson@bardsley-england.com>
2021-05-13 12:26:29 +0100

    feat: deployment package
    Co-authored-by: Richard Pryce

--- deploy/index.ts
+++ deploy/index.ts
@@ -3,7 +3,7 @@ import * as aws from "@pulumi/aws";
 import { Runtime } from "@pulumi/aws/lambda";
 import { ComponentResource, Output, ResourceOptions } from "@pulumi/pulumi";
 import { Tags } from "@pulumi/aws";
-import { getSoilSample } from "@bx-looop/soil-domain-service-runtime";
+import { getSoilSample, saveSoilSample } from "@bx-looop/soil-domain-service-runtime";
 import { buildTags } from "./lib";
 
 type SoilDomainServiceProps = {
@@ -13,6 +13,10 @@ type SoilDomainServiceProps = {
 
 export class SoilDomainService extends ComponentResource {
   readonly lambdaArn: Output<string>;
+
+  readonly bucketName: Output<string>;
+
+  readonly tableName: Output<string>;
 
   constructor(name: string, args: SoilDomainServiceProps, opts?: ResourceOptions) {
     super("bx:components:SoilDomainService", name, args, opts);
@@ -27,14 +31,53 @@ export class SoilDomainService extends ComponentResource {
     // it refers to the imported library function and does not duplicate the function body
     type CallBackParameters = Parameters<typeof getSoilSample>;
 
-    const callbackFunction = new aws.lambda.CallbackFunction(
-      "lambdaFunction",
+    const dynamoTable = new aws.dynamodb.Table("soilSample", {
+      attributes: [
+        {
+          name: "id",
+          type: "S",
+        },
+        {
+          name: "orchardId",
+          type: "S",
+        },
+        {
+          name: "sampleDate",
+          type: "S",
+        },
+      ],
+      billingMode: "PROVISIONED",
+      globalSecondaryIndexes: [
+        {
+          hashKey: "orchardId",
+          name: "orchardId-sampleDate-index",
+          nonKeyAttributes: ["id"],
+          projectionType: "INCLUDE",
+          rangeKey: "sampleDate",
+          readCapacity: 2,
+          writeCapacity: 2,
+        },
+      ],
+      hashKey: "id",
+      rangeKey: "orchardId",
+      readCapacity: 2,
+      tags: {},
+      writeCapacity: 2,
+    });
+
+    const getSoilSampleLambda = new aws.lambda.CallbackFunction(
+      "getSoilSample",
       {
         callback: (e: CallBackParameters[0]) => {
           return getSoilSample(e);
         },
         codePathOptions: {
           extraExcludePackages,
+        },
+        environment: {
+          variables: {
+            SOIL_SAMPLE_TABLE: dynamoTable.name,
+          },
         },
         description: `getSoilSample handler for ${name}`,
         memorySize: 128,
@@ -45,7 +88,38 @@ export class SoilDomainService extends ComponentResource {
       { parent: this }
     );
 
-    this.lambdaArn = callbackFunction.arn;
+    const s3Bucket = new aws.s3.Bucket("eurofinsData", {
+      acl: "private",
+    });
+
+    const saveSoilSampleLambda = new aws.lambda.CallbackFunction(
+      "saveSoilSample",
+      {
+        callback: (e: CallBackParameters[0]) => {
+          return saveSoilSample(e);
+        },
+        codePathOptions: {
+          extraExcludePackages,
+        },
+        environment: {
+          variables: {
+            SOIL_SAMPLE_TABLE: dynamoTable.name,
+          },
+        },
+        description: `getSoilSample handler for ${name}`,
+        memorySize: 128,
+        runtime: Runtime.NodeJS14dX,
+        tags,
+        timeout: 5,
+      },
+      { parent: this }
+    );
+
+    s3Bucket.onObjectCreated("s3ObjectCreated", saveSoilSampleLambda);
+
+    this.bucketName = s3Bucket.bucket;
+    this.tableName = dynamoTable.name;
+    this.lambdaArn = getSoilSampleLambda.arn;
     this.registerOutputs();
   }
 }
--- example/index.ts
+++ example/index.ts
@@ -1,9 +1,14 @@
-import {SoilDomainService} from "@bx-looop/soil-domain-service";
+import { SoilDomainService } from "@bx-looop/soil-domain-service";
 
 export = async () => {
-  const tags = {"example": "this is an example"}
-  const service = new SoilDomainService("example",{
-    lambdaExcludePackages:[],
-    tags
+  const tags = { example: "this is an example" };
+  const service = new SoilDomainService("example", {
+    lambdaExcludePackages: [],
+    tags,
   });
+  return {
+    bucketName: service.bucketName,
+    lambdaArn: service.lambdaArn,
+    tableName: service.tableName,
+  };
 };
